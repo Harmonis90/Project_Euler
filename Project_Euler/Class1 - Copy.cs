250e9ff019ab97dacb937bf9cca54c45685944c0
drpep <drpep@DESKTOP-4LMT73U>
2021-09-03 17:49:54 -0400

    Add project files.

---
+++ Project_Euler/Class1 - Copy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class PrimeFactor
+{
+    /*The prime factors of 13195 are 5, 7, 13 and 29.
+        
+        What is the largest prime factor of the number 600851475143 ?*/
+    public static void ProblemThree(long n)
+    {
+        List<long> primeFactors = new List<long>();
+
+        for(int i = 3; i <= Math.Sqrt(n); i += 2)
+        {
+            while(n % i == 0)
+            {
+                primeFactors.Add(i);
+                n /= i;
+            }
+           
+        }
+        primeFactors.Add(n);
+      
+        if (n > 2)
+        {
+            Console.WriteLine("The Answer is {0}", primeFactors[primeFactors.Count - 1]);
+        }
+       
+    }
+    public static bool IsPrime(int n)
+    {
+        
+        if (n == 0) { return false; }
+        if (n == 1) { return false; }
+        if (n == 2) { return true; }
+        if (n % 2 == 0) { return false; }
+        for(int i = 3; i < n / 2; i += 2)
+        {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
---
+++ Project_Euler/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Project_Euler
+{
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            PrimeFactor.ProblemThree(600851475143);
+            
+            
+        }
+        /*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+        Find the sum of all the multiples of 3 or 5 below 1000.*/
+        static int ProblemOne(int maxRange)
+        {
+            int result = 0;
+            for (int i = 0; i < maxRange; i++)
+            {
+                if(i % 3 == 0 || i % 5 == 0)
+                {
+                    result += i;
+                }
+            }
+            Console.WriteLine(result);
+            return result;
+        }
+        /*By considering the terms in the Fibonacci sequence whose values do not exceed four million, 
+     * find the sum of the even-valued terms.*/
+        static int ProblemTwo()
+        {
+            int result = 0;
+            int evensResult = 0;
+            int counter = 0;
+            int stop = 4000000; 
+            while (true)
+            {
+                if(result < stop)
+                {
+                    result = Fibonacci(counter);
+                    if(result % 2 == 0)
+                    {
+                        evensResult += result;
+                    }
+                    counter++;
+                }
+                else
+                {
+                    counter--;
+                    break;
+                }
+            }
+            Console.WriteLine(evensResult);
+            return counter;
+        }
+        static int Fibonacci(int num)
+        {
+            int firstNum = 0;
+            int secondNum = 1;
+            int result = 0;
+            if(num == 0)
+            {
+                return 0;
+            }
+            else if(num == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                for (int i = 2; i <= num; i++)
+                {
+                    result = firstNum + secondNum;
+                    firstNum = secondNum;
+                    secondNum = result;
+                    
+                }
+                Console.WriteLine(result);
+                return result;
+            }
+        }
+        
+    }
+
+
+}
